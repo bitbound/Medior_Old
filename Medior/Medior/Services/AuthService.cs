10764f76e74f8664c20e8feddbf96ad37f9a9b60
Jared Goodwin <translucency@outlook.com>
2021-11-12 17:21:49 -0800

    Microsoft Identity Platform flows working.

--- Medior/Medior/Services/AuthService.cs
+++ Medior/Medior/Services/AuthService.cs
@@ -1,4 +1,5 @@
-﻿using Medior.Utilities;
+﻿using Medior.Core.Shared.BaseTypes;
+using Medior.Utilities;
 using Microsoft.Extensions.Logging;
 using Microsoft.Identity.Client;
 using System;
@@ -9,36 +10,37 @@ using System.Threading.Tasks;
 
 namespace Medior.Services
 {
-    public class AuthService
+    public interface IAuthService
     {
-        private static readonly string _tenantName = "fabrikamb2c";
-        private static readonly string _tenant = $"{_tenantName}.onmicrosoft.com";
+        Task<Result<AuthenticationResult>> GetTokenSilently(IntPtr windowHandle);
+
+        Task<Result<AuthenticationResult>> SignInInteractive(IntPtr windowHandle);
+    }
+
+    public class AuthService : IAuthService
+    {
         private static readonly string _azureAdB2CHostname = $"{_tenantName}.b2clogin.com";
-
-        private static readonly string _clientId = "841e1190-d73a-450c-9d68-f5cf16b78e81";
-
+        private static readonly string _clientId = "1d50d95c-e211-4499-9248-fa4e5dbff323";
         private static readonly string _redirectUri = $"https://{_tenantName}.b2clogin.com/oauth2/nativeclient";
-
-        private static string _policySignUpSignIn = "b2c_1_susi";
-        private static string _policyEditProfile = "b2c_1_edit_profile";
-        private static string _policyResetPassword = "b2c_1_reset";
-
-        private static string[] _apiScopes = { $"https://{_tenant}/helloapi/demo.read" };
-
+        private static readonly string _tenant = $"{_tenantName}.onmicrosoft.com";
+        private static readonly string _tenantName = "mediorapp";
+        private static string[] _apiScopes = { $"https://{_tenant}/medior-api/app.user" };
         private static string _authorityBase = $"https://{_azureAdB2CHostname}/tfp/{_tenant}/";
-        private static string _authoritySignUpSignIn = $"{_authorityBase}{_policySignUpSignIn}";
         private static string _authorityEditProfile = $"{_authorityBase}{_policyEditProfile}";
         private static string _authorityResetPassword = $"{_authorityBase}{_policyResetPassword}";
-
+        private static string _authoritySignUpSignIn = $"{_authorityBase}{_policySignUpSignIn}";
+        private static string _policyEditProfile = "b2c_1_edit_profile";
+        private static string _policyResetPassword = "b2c_1_reset_password";
+        private static string _policySignUpSignIn = "b2c_1_signup_signin";
         private readonly ILogger<AuthService> _logger;
 
-        public IPublicClientApplication PublicClientApp { get; private set; }
+        private readonly IPublicClientApplication _publicClientApp;
 
         public AuthService(ILogger<AuthService> logger)
         {
             _logger = logger;
 
-            PublicClientApp = PublicClientApplicationBuilder.Create(_clientId)
+            _publicClientApp = PublicClientApplicationBuilder.Create(_clientId)
                 .WithB2CAuthority(_authoritySignUpSignIn)
                 .WithRedirectUri(_redirectUri)
 #if DEBUG
@@ -48,9 +50,76 @@ namespace Medior.Services
 #endif
                 .Build();
 
-            TokenCacheHelper.Bind(PublicClientApp.UserTokenCache);
+            TokenCacheHelper.Bind(_publicClientApp.UserTokenCache);
         }
 
+        public async Task<Result<AuthenticationResult>> GetTokenSilently(IntPtr windowHandle)
+        {
+            AuthenticationResult? authResult = null;
+            var accounts = await _publicClientApp.GetAccountsAsync(_policySignUpSignIn);
+
+            try
+            {
+                authResult = await _publicClientApp
+                    .AcquireTokenSilent(_apiScopes, accounts.FirstOrDefault())
+                    .ExecuteAsync();
+
+                return Result.Ok(authResult);
+            }
+            catch (MsalUiRequiredException ex)
+            {
+                _logger.LogError(ex, "Failed to acquire token silently. Interactive logon required.");
+                return await SignInInteractive(windowHandle);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to acquire token silently.");
+            }
+
+            return Result.Fail<AuthenticationResult>("Failed to acquire token silently.");
+        }
+
+        public async Task<Result<AuthenticationResult>> SignInInteractive(IntPtr windowHandle)
+        {
+            AuthenticationResult? authResult = null;
+            try
+            {
+                authResult = await _publicClientApp.AcquireTokenInteractive(_apiScopes)
+                    .WithParentActivityOrWindow(windowHandle)
+                    .ExecuteAsync();
+            }
+            catch (MsalException ex)
+            {
+                try
+                {
+                    _logger.LogError(ex, "Error acquiring token.");
+
+                    if (ex.Message.Contains("AADB2C90118"))
+                    {
+                        authResult = await _publicClientApp.AcquireTokenInteractive(_apiScopes)
+                            .WithParentActivityOrWindow(windowHandle)
+                            .WithPrompt(Prompt.SelectAccount)
+                            .WithB2CAuthority(_authorityResetPassword)
+                            .ExecuteAsync();
+                    }
+                }
+                catch (Exception ex2)
+                {
+                    _logger.LogError(ex2, "Error acquiring token.");
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error acquiring token.");
+            }
+
+            if (authResult is null)
+            {
+                return Result.Fail<AuthenticationResult>("Failed to acquire auth token.");
+            }
+
+            return Result.Ok(authResult);
+        }
         private void Log(Microsoft.Identity.Client.LogLevel level, string message, bool containsPii)
         {
             if (message is null)
