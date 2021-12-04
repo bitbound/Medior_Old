33684dbddb3d8b50f448f1c1b3f224f4adaccd99
Jared Goodwin <translucency@outlook.com>
2021-12-04 09:02:02 -0800

    Update About page.  Update package capabilities.

--- Medior/Medior/Pages/AboutPage.xaml.cs
+++ Medior/Medior/Pages/AboutPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.UI.Xaml.Controls;
+using Windows.System;
 
 namespace Medior.Pages
 {
@@ -13,5 +14,10 @@ namespace Medior.Pages
         }
 
         public string? Version => typeof(AboutPage)?.Assembly?.GetName()?.Version?.ToString();
+
+        private async void ContactHyperlink_Click(Microsoft.UI.Xaml.Documents.Hyperlink sender, Microsoft.UI.Xaml.Documents.HyperlinkClickEventArgs args)
+        {
+            await Launcher.LaunchUriAsync(new Uri("mailto:hello@lucency.co?subject=Message from Medior App"));
+        }
     }
 }
