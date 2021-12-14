c168c36a6e5368055d6d6a3eaa3ab10468ac6448
Jared Goodwin <translucency@outlook.com>
2021-12-14 12:46:36 -0800

    Create directories if missing.

--- Medior/Medior/Utilities/AppFolders.cs
+++ Medior/Medior/Utilities/AppFolders.cs
@@ -4,11 +4,22 @@ namespace Medior.Utilities
 {
     public static class AppFolders
     {
-        public static string AppData => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Medior");
-        public static string LogsPath => Path.Combine(TempPath, "Logs");
-        public static string PhotoSorterLogsPath => Path.Combine(TempPath, "PhotoSorter");
-        public static string RecordingsPath => Path.Combine(TempPath, "Recordings");
-        public static string TempBin => Path.Combine(TempPath, "bin");
-        public static string TempPath => Path.Combine(Path.GetTempPath(), "Medior");
+        public static string AppData =>
+            Directory.CreateDirectory(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Medior")).FullName;
+
+        public static string LogsPath =>
+            Directory.CreateDirectory(Path.Combine(TempPath, "Logs")).FullName;
+
+        public static string PhotoSorterLogsPath =>
+            Directory.CreateDirectory(Path.Combine(TempPath, "PhotoSorter")).FullName;
+
+        public static string RecordingsPath =>
+            Directory.CreateDirectory(Path.Combine(TempPath, "Recordings")).FullName;
+
+        public static string TempBin =>
+            Directory.CreateDirectory(Path.Combine(TempPath, "bin")).FullName;
+
+        public static string TempPath =>
+            Directory.CreateDirectory(Path.Combine(Path.GetTempPath(), "Medior")).FullName;
     }
 }
